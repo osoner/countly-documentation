5b7877f87f132f7b353c7ba77a62849c0234c422
Görkem Çetin <gc@count.ly>
2013-04-07 01:57:11 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -13,19 +13,19 @@ var menu = [
         name:"Reference",
         label:"reference",
         items:[
-            { link:"user-guide", name:"User Guide" },
+            { link:"user-guide", name:"User guide" },
             { link:"features", name:"Features" },
             { link:"changelog", name:"Changelog" },
             { link:"roadmap", name:"Roadmap" },
             { link:"server-api", name:"Server API" },
             { link:"contributors", name:"Contributors" },
-            { link:"custom-events", name:"Custom Events" },
-            { link:"sdk-methods-for-custom-events", name:"SDK Methods for Custom Events" },
-            { link:"implementation-scenarios", name:"Implementation Scenarios" },
-            { link:"increasing-performance", name:"Increasing Performance" },
-            { link:"translating-countly", name:"Translating Countly" },
+            { link:"custom-events", name:"Custom events" },
+            { link:"sdk-methods-for-custom-events", name:"Custom events SDK methods" },
+            { link:"implementation-scenarios", name:"Implementation scenarios" },
+            { link:"increasing-performance", name:"Increasing performance" },
+            { link:"translating-countly", name:"Translating countly" },
             { link:"glossary", name:"Glossary" },
-            { link:"useful-links", name:"Useful Links" }            
+            { link:"useful-links", name:"Useful links" }            
         ]
     },
      {
